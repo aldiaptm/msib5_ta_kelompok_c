26aec5462076af991adbe22ceedca66198f8ccb0
fawwazaziz123A <fawwazazizxtkj2@gmial.com>
2023-11-28 10:47:55 +0700

    menambahkan sebuat alert

--- travelling/main_page/proses_tambah_contact.php
+++ travelling/main_page/proses_tambah_contact.php
@@ -1,23 +1,22 @@
 <?php
 include '../koneksi.php';
-date_default_timezone_set('asia/Jakarta');
+date_default_timezone_set('Asia/Jakarta');
 
-// get variable from form inputt
-$nama_contact = $_POST["nama_contact"];
-$subjek = $_POST["subjek"];
-$pesan = $_POST["pesan"];
+// get variable from form input
+$nama_contact = mysqli_real_escape_string($koneksi, $_POST["nama_contact"]);
+$subjek = mysqli_real_escape_string($koneksi, $_POST["subjek"]);
+$pesan = mysqli_real_escape_string($koneksi, $_POST["pesan"]);
 $tanggal_contact = date('Y-m-d h:i:s');
 
 $query = "INSERT INTO `contact` (`nama_contact`, `subjek`, `pesan`, `tanggal_contact`) VALUES ('$nama_contact', '$subjek', '$pesan', '$tanggal_contact')";
 $result = mysqli_query($koneksi, $query);
 
 if ($result) {
-    echo "Pesan berhasil dikirim!";
+    // Data berhasil dimasukkan, tampilkan alert dan kembali ke halaman contact
+    echo '<script>alert("Pesan berhasil dikirim!"); window.location.href = "contact.php";</script>';
+    die(); // tambahkan ini jika diperlukan
 } else {
     echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+    header("location:contact.php");
 }
-
-
-header("location:contact.php");
-
 ?>
