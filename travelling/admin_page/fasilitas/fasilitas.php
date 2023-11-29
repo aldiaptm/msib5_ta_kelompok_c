7d06537806a1e0dc5a9f3e660e659ccc9e50d39b
aldiaptm <aldiprtm16@gmail.com>
2023-11-29 10:57:05 +0700

    mengedit sedikit

--- travelling/admin_page/fasilitas/fasilitas.php
+++ travelling/admin_page/fasilitas/fasilitas.php
@@ -106,7 +106,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="../tables.php">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Destinasi</span></a>
             </li>
 
             <!-- Divider -->
