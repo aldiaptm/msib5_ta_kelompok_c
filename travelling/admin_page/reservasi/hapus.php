09baf6b6271910d6ada07288db117c818d81d984
aldiaptm <aldiprtm16@gmail.com>
2023-11-29 13:36:54 +0700

    hapus reservasi

--- travelling/admin_page/reservasi/hapus.php
+++ travelling/admin_page/reservasi/hapus.php
@@ -1,7 +1,7 @@
 <?php 
 include '../../koneksi.php';
-$result = mysqli_query($koneksi, "DELETE from customer where `id_customer` = '$_GET[id_customer]'");
+$result = mysqli_query($koneksi, "DELETE from reservasi where `id_reservasi` = '$_GET[id_reservasi]'");
 
-echo "<script>alert('Data berhasil dihapus.'); window.location.href='customer.php';</script>";
+echo "<script>alert('Data berhasil dihapus.'); window.location.href='reservasi.php';</script>";
 
 ?>
